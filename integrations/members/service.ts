ccf620aca01143b0a18ad46667fe963c54513955
eacollectibles <ea.collectibles.service@gmail.com>
2026-01-17 00:58:01 -0500

    Handle case when member object is missing

--- integrations/members/service.ts
+++ integrations/members/service.ts
@@ -4,8 +4,9 @@ import { Member } from ".";
 export const getCurrentMember = async (): Promise<Member | null> => {
   try {
     const member = await members.getCurrentMember({ fieldsets: ["FULL"] });
-    if (!member) {
+    if (!member || !member.member) {
       console.log('==== No member found');
+      return null;
     }
     return member.member;
   } catch (error) {
