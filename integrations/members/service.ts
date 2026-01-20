fb3584c1bf4c308c29e30ed323f1459fc2f6e96f
Wix Remote Machine <support@wix.com>
2026-01-20 02:15:44 +0000

    Template updated

--- integrations/members/service.ts
+++ integrations/members/service.ts
@@ -4,9 +4,8 @@ import { Member } from ".";
 export const getCurrentMember = async (): Promise<Member | null> => {
   try {
     const member = await members.getCurrentMember({ fieldsets: ["FULL"] });
-    if (!member || !member.member) {
+    if (!member) {
       console.log('==== No member found');
-      return null;
     }
     return member.member;
   } catch (error) {
