5bf9ec720f0fd224ff5d5f0052a47a20fbf1e060
eacollectibles <ea.collectibles.service@gmail.com>
2026-01-21 22:04:38 -0500

    Update Head.tsx
    changes

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -201,10 +201,11 @@ export const Head = () => {
       <link rel="manifest" href="/site.webmanifest" />
       
       {/* Favicon and Icons */}
-      <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+<link rel="shortcut icon" href="/favicon.ico" />
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+```
       
       {/* Default OG Image (overridden by AutoSEO per page) */}
       <meta property="og:image" content="https://legalassist.london/og-image.jpg" />
