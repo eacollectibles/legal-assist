67409bf85e10b4f278c0e30d3a50f3baecd0e272
eacollectibles <ea.collectibles.service@gmail.com>
2026-01-24 02:03:49 -0500

    Update Router.tsx
    chganges

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -13,6 +13,8 @@ const AboutPage = lazy(() => import('@/components/pages/AboutPage'));
 const ServicesPage = lazy(() => import('@/components/pages/ServicesPage'));
 const ClientSignupPage = lazy(() => import('@/components/pages/ClientSignupPage'));
 const ClientLoginPage = lazy(() => import('@/components/pages/ClientLoginPage'));
+const ClientIntakePage = lazy(() => import('@/components/pages/ClientIntakePage'));
+const ClientDashboardPage = lazy(() => import('@/components/pages/ClientDashboardPage'));
 
 // Service Pages
 const SmallClaimsPage = lazy(() => import('@/components/pages/SmallClaimsPage'));
@@ -178,6 +180,8 @@ export default function Router() {
             <Route path="/services" element={<ServicesPage />} />
             <Route path="/client-signup" element={<ClientSignupPage />} />
             <Route path="/client-login" element={<ClientLoginPage />} />
+            <Route path="/client-intake" element={<ClientIntakePage />} />
+            <Route path="/client-dashboard" element={<ClientDashboardPage />} />
 
             {/* Core Service Pages */}
             <Route path="/services/small-claims-court" element={<SmallClaimsPage />} />
