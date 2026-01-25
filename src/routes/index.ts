93b6d7a4d886d6c9c6c8ef848d2357a521f3cc61
eacollectibles <ea.collectibles.service@gmail.com>
2026-01-25 00:47:48 -0500

    Create index.ts
    dsfgsdvfsdf

---
+++ src/routes/index.ts
@@ -0,0 +1,6 @@
+export { trafficRoutes } from './trafficRoutes';
+export { landlordTenantRoutes } from './landlordTenantRoutes';
+export { smallClaimsRoutes } from './smallClaimsRoutes';
+export { locationRoutes } from './locationRoutes';
+export { otherServiceRoutes } from './otherServiceRoutes';
+export { guideRoutes } from './guideRoutes';
